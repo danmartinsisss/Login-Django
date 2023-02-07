f9ac0b65edc272b5c7b3d53b910baaff6e8a6b03
Daniel Martins <40578931+danmartinsisss@users.noreply.github.com>
2023-02-07 17:07:23 -0400

    Add files via upload
    Atualization!!

---
+++ models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_of_birth = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to='users/%d/%m/%Y/', blank=True)
+
+    def __str__(self):
+        return f'Perfil de {self.user.username}'
