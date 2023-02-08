c4e2ab5948f5744f7dc19648331efc64a4c01f1e
Daniel Martins <40578931+danmartinsisss@users.noreply.github.com>
2023-02-08 09:43:22 -0400

    Add files via upload
    Now you have to  install a certificate to use that files. And add 127.0.0.1 to hosts, Include the Google authentication, you ave only to create a secretekey on your admin panel at google cloud.

---
+++ account/authentication.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import User
+from account.models import Profile
+
+def create_profile(backend, user, *args, **kwargs):
+    # Criar um perfil ara o usuario com autenticacao via rede social
+    Profile.objects.get_or_create(user=user)
+
+
+class EmailAuthBackend:
+    # Autenticando usando um email
+    def authenticate(self, request, username=None, password=None):
+        try:
+            user = User.objects.get(email=username)
+            if user.check_password(password):
+                return user
+            return None
+        except (User.DoesNotExist, User.MultipleObjectsReturned):
+            return None
+        
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
+        
--- account/views.py
+++ account/views.py
@@ -1,29 +1,72 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
 from django.contrib.auth.decorators import login_required
-
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
+from .models import Profile
+from django.contrib import messages
 # Create your views here.
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            new_user.set_password(
+                user_form.cleaned_data['password'])
+            # Save the User object
+            new_user.save()
+            Profile.objects.create(user=new_user)
+            return render(request, 'account/register_done.html',{'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form': user_form})
 
 def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(request, username=cd['username'], password=cd['password'])
+            user = authenticate(request, 
+                                username=cd['usuario'], 
+                                password=cd['senha'])
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return HttpResponse('Authentication successful')
+                    return HttpResponse('Autenticado com sucesso')
                 else:
-                    return HttpResponse('Disabled account')
+                    return HttpResponse('Conta inativa')
             else:
-                return HttpResponse('Invalid username or password')
+                return HttpResponse('login invalido')
     else:
         form = LoginForm()
-    return render(request, 'account/login.html', {'form': form})
+    return render(request, 'account/login.html',{'form':form})
+
 
 @login_required
 def dashboard(request):
-    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+    return render(request, 'account/dashboard.html',{'section': 'dashboard'})
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST, files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Perfil alterado com sucesso')
+        else:
+            messages.error(request, 'Erro ao atualizar seu perfil')
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)   
+    
+    return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form': profile_form})
+
+
+
+
+
